4ce75a08cdd5b5e166b8d73a611ad52d1180b88b
何及 <7721482+what_and@user.noreply.gitee.com>
2020-11-09 11:20:50 +0800

    row和col初步完成

--- src/app.js
+++ src/app.js
@@ -1,63 +1,18 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
-import buttonGroup from './button-group.vue'
+import ButtonGroup from './button-group.vue'
+import Row from './row.vue'
+import Col from './col.vue'
+
+
 Vue.component('w-button',Button);
 Vue.component('w-icon',Icon);
-Vue.component('w-button-group',buttonGroup);
+Vue.component('w-button-group',ButtonGroup);
+Vue.component('w-row',Row);
+Vue.component('w-col',Col);
+
 
 new Vue({
     el: '#app'
 })
-
-import Chai from 'chai'
-//单元测试
-{
-    //测试按钮含有 icon
-    const div = document.createElement('div');
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings'
-        }
-    });
-    vm.$mount(div);
-    let useElement = vm.$el.querySelector('use');
-    Chai.expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')  
-    vm.$el.remove();
-    vm.$destroy();
-}
-{
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings'
-        }
-    });
-    vm.$mount(div);
-    let svg = vm.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg);
-    Chai.expect(order).to.eq("1");
-    vm.$el.remove();
-    vm.$destroy();
-}
-{
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData:{
-            icon: 'settings',
-            iconPosition: 'right'
-        }
-    });
-    vm.$mount(div);
-    let svg = vm.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg);
-    Chai.expect(order).to.eq("2");
-    vm.$el.remove();
-    vm.$destroy();
-}
---
+++ src/col.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        span: {
+            type: [Number,String]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        background: grey;
+        width: 50%;
+        border: 1px solid red;
+
+        $class: col-;
+        @for $n from 1 through 12{
+            &.#{$class}#{$n}{
+                width: ($n / 12) * 100%;
+            }
+        }
+        
+     }
+       
+</style>
