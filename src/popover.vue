fc37e2423c18279ad7371849fd770ab3fe29ee88
何及 <7721482+what_and@user.noreply.gitee.com>
2020-11-13 11:23:21 +0800

    解决当容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
  <div class="popover" @click.stop="xxx">
-    <div v-if="visible" class="content-wrapper" @click.stop>
+    <div ref="contentWrapper" v-if="visible" class="content-wrapper">
         <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+        <slot></slot>
+    </span>
  </div>
 </template>
  
@@ -18,15 +20,22 @@ export default {
         xxx(){
             this.visible = !this.visible;
             if(this.visible === true){
-                setTimeout(()=>{
+                this.$nextTick(()=>{
+                    document.body.appendChild(this.$refs.contentWrapper)
+                    let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect();
+                    this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+                    this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
                     let eventHandler = () => {
                         this.visible = false;
                         document.removeEventListener('click',eventHandler)             
                     }
                     document.addEventListener('click',eventHandler);
-                },0)                    
+                })                    
             }
         }
+    },
+    mounted(){
+        console.log(this.$refs.triggerWrapper);
     }
 }
 </script>
@@ -36,9 +45,10 @@ export default {
         display: inline-block;
         vertical-align: top;
         position: relative;
-        .content-wrapper{
-           position:  absolute; 
-           bottom: 100%;
-        }
+        height: 100%;
     }
+    .content-wrapper{
+        position:  absolute; 
+        transform: translateY(-100%);
+     }
 </style>
