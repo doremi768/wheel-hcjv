ed9354e14116ba6da62406682cd35a4b575d229d
何及 <7721482+what_and@user.noreply.gitee.com>
2020-11-22 16:07:39 +0800

    更新单元测试

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,53 @@
+import { expect } from 'chai'
+import Vue from 'vue'
+import { shallowMount, mount } from '@vue/test-utils'
+import Button from '@/button.vue'
+
+describe('Button.vue', () => {
+    it('存在.', () => {
+        expect(Button).to.be.ok 
+    })
+    it('可以设置icon.', () => {
+        const wrapper = mount(Button,{
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        const useElement = wrapper.find('use');
+        console.log(useElement.attributes());
+        
+        expect(useElement.attributes()['href']).to.equal('#i-settings')
+    })
+    it('可以设置loading.', () => {
+        const wrapper = mount(Button,{
+            propsData: {
+                icon: 'loading'
+            }
+        })
+        const useElement = wrapper.find('use');
+        expect(useElement.attributes()['href']).to.equal('#i-loading')
+    })
+    it('icon 默认的 order 是 1', () => {
+        const wrapper = mount(Button,{
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        // expect(getComputedStyle(icon.selector).order).to.eq('1')
+    })
+    it('设置 iconPosition 可以改变 order', () => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+        }).$mount(div)
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    })
+})
